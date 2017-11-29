2a52fab14f15032ac565d460c5b46f3f5036490c
Alexey Kamenskiy <alexey.kamenskiy@chinanetcloud.com>
2017-11-29 15:01:30 +0800

    Simple commands example

---
+++ examples/commands/commands.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/akamensky/argparse"
+	"os"
+)
+
+// Run this as `go run commands.go [start|stop]`
+func main() {
+	// Create new parser object
+	parser := argparse.NewParser("commands", "Simple example of argparse commands")
+
+	// Add top level command `start`
+	startCmd := parser.NewCommand("start", "Will start a process")
+
+	// Add top level commands `stop`
+	stopCmd := parser.NewCommand("stop", "Will stop a process")
+
+	// Parse command line arguments and in case of any error print error and help information
+	err := parser.Parse(os.Args)
+	if err != nil {
+		fmt.Println(err.Error())
+		fmt.Print(parser.Usage())
+		return
+	}
+
+	// Check if `start` command was given
+	if startCmd.Happened() {
+		// Starting a process
+		fmt.Println("Started process")
+	} else if stopCmd.Happened() { // Check if `stop` command was given
+		// Stopping a process
+		fmt.Println("Stopped process")
+	} else {
+		// In fact we will never hit this one
+		// because commands and sub-commands are considered as required
+		fmt.Println(fmt.Errorf("bad arguments, please check usage"))
+		fmt.Print(parser.Usage())
+	}
+}
