b0b751b9cdb9fe3fe85f4d3ed1c883e2d4fe4b27
Alexey Kamenskiy <alexey.kamenskiy@chinanetcloud.com>
2017-12-01 11:47:30 +0800

    Advanced commands example

---
+++ examples/commands-advanced/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"github.com/akamensky/argparse"
+	"github.com/akamensky/argparse/examples/commands-advanced/zoo"
+	"log"
+	"os"
+)
+
+func main() {
+	// Create new parser object
+	parser := argparse.NewParser("zooprog", "Program that walks us through the zoo")
+
+	// Note that we create argument off the parser, so it will work on all scopes
+	// also this is required argument, meaning we cannot avoid but to always provide it
+	name := parser.String("", "name", &argparse.Options{Help: "Provide an optional name for the animal", Required: true})
+
+	// dog command
+	dogCmd := parser.NewCommand("dog", "We are going to see dog")
+	// dog sub-commands
+	dogSpeak := dogCmd.NewCommand("speak", "Make the dog speak")
+	dogFeed := dogCmd.NewCommand("feed", "Make the dog eat")
+	dogSummon := dogCmd.NewCommand("speak", "Make the dog come over")
+	dogPlay := dogCmd.NewCommand("speak", "Make the dog play")
+
+	// cat command
+	catCmd := parser.NewCommand("cat", "We are going to see cat")
+	// cat sub-commands
+	catSpeak := catCmd.NewCommand("speak", "Make the cat speak")
+	catFeed := catCmd.NewCommand("feed", "Make the cat eat")
+	catSummon := catCmd.NewCommand("speak", "Make the cat come over")
+	catPlay := catCmd.NewCommand("speak", "Make the cat play")
+
+	// Optional argument for dog.
+	// Note that we create this argument for dogCmd, which means catCmd will not have this argument
+	wiggleFlag := dogCmd.Flag("", "wiggle", &argparse.Options{Help: "Makes the dog to wiggle its tail"})
+
+	// Now parse the arguments
+	err := parser.Parse(os.Args)
+	if err != nil {
+		fmt.Println(err.Error())
+		fmt.Println(parser.Usage())
+		return
+	}
+
+	// When there is no name provided
+	// we need to give default value
+	if *name == "" {
+		*name = "unnamed"
+	}
+
+	// If user gave `dog` command
+	if dogCmd.Happened() {
+		// Get a dog and give it a name
+		animal := zoo.NewDog(*name)
+
+		if dogSpeak.Happened() {
+			animal.Do("speak")
+		} else if dogFeed.Happened() {
+			animal.Do("feed")
+		} else if dogSummon.Happened() {
+			animal.Do("summon")
+		} else if dogPlay.Happened() {
+			animal.Do("play")
+		} else {
+			// This should be unreachable
+			log.Fatal("Uh-oh, unknown command! Impossible! Ex-ter-mi-nate!")
+		}
+
+		// If we got wiggle flag, then well... wiggle, ok?
+		if *wiggleFlag {
+			animal.WiggleTail()
+		}
+	} else if catCmd.Happened() {
+		// Get a cat and give it a name
+		animal := zoo.NewCat(*name)
+
+		if catSpeak.Happened() {
+			animal.Do("speak")
+		} else if catFeed.Happened() {
+			animal.Do("feed")
+		} else if catSummon.Happened() {
+			animal.Do("summon")
+		} else if catPlay.Happened() {
+			animal.Do("play")
+		} else {
+			// This should be unreachable
+			log.Fatal("Uh-oh, unknown command! Impossible! Ex-ter-mi-nate!")
+		}
+	} else {
+		// This should be unreachable
+		log.Fatal("Uh-oh, something weird happened")
+	}
+}
---
+++ examples/commands-advanced/zoo/zoo.go
@@ -0,0 +1,96 @@
+package zoo
+
+import (
+	"fmt"
+	"gitlab.service.chinanetcloud.com/internal-products/remote-config-manager/rcm-agent/log"
+)
+
+var actions = map[string]int{
+	"speak":  1,
+	"feed":   2,
+	"summon": 3,
+	"play":   4,
+}
+
+func GetActions() []string {
+	result := make([]string, 0, len(actions))
+	for key := range actions {
+		result = append(result, key)
+	}
+
+	return result
+}
+
+type animal struct {
+	Name   string
+	says   string
+	feed   string
+	summon string
+	play   string
+}
+
+func (o *animal) Do(action string) {
+	switch actions[action] {
+	case 1:
+		o.doSay()
+	case 2:
+		o.doFeed()
+	case 3:
+		o.doSummon()
+	case 4:
+		o.doPlay()
+	default:
+		log.Fatal("Wow, we got unknown action, that should have never happened")
+	}
+}
+
+func (o *animal) doSay() {
+	fmt.Println(o.says)
+}
+
+func (o *animal) doFeed() {
+	fmt.Println(o.feed)
+}
+
+func (o *animal) doSummon() {
+	fmt.Println(o.summon)
+}
+
+func (o *animal) doPlay() {
+	fmt.Println(o.play)
+}
+
+type dog struct {
+	animal
+}
+
+// Dog has an extra action to wiggle its tail
+func (o *dog) WiggleTail() {
+	fmt.Println("* Dog wiggles its tail and stares at you with love")
+}
+
+type cat struct {
+	animal
+}
+
+func NewDog(name string) dog {
+	result := new(dog)
+	result.Name = name
+	result.says = "Woof"
+	result.feed = "* Dog eats the food and stays now happily wiggles it tail"
+	result.summon = "* Dog immediately shows up wiggling its tail"
+	result.play = "* Dog runs around in excitement"
+
+	return *result
+}
+
+func NewCat(name string) cat {
+	result := new(cat)
+	result.Name = name
+	result.says = "Meow"
+	result.feed = "* Cat eats the food and slowly walks away"
+	result.summon = "* Cat is nowhere to be found"
+	result.play = "* Cat stares at you in disgust for awhile and goes back to sleep"
+
+	return *result
+}
