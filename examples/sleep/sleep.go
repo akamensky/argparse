b837a6672094ce0a43724b99950c88c6f80d4f5f
Jaikrishna Tanjore Sukumar <JaikrishnaTS@users.noreply.github.com>
2018-04-04 22:52:31 -0400

    Example for Int, Float and Default

---
+++ examples/sleep/sleep.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/akamensky/argparse"
+	"os"
+	"time"
+)
+
+func main() {
+	// Create new parser object
+	parser := argparse.NewParser("flags", "Simple example of argparse flags")
+
+	// Create count integer argument
+	count := parser.Int("c", "count", &argparse.Options{Required: true, Help: "Number of iterations to count"})
+
+	// Create delay float argument
+	delay := parser.Float("d", "delay", &argparse.Options{Default: 1.0, Help: "Delay between iterations"})
+
+	// Parse input
+	err := parser.Parse(os.Args)
+	if err != nil {
+		// In case of error print error and print usage
+		// This can also be done by passing -h or --help flags
+		fmt.Print(parser.Usage(err))
+	}
+
+	// Print (*count) iterations with (*delay) second delays
+	delay_duration := time.Duration(*delay) * time.Second
+	for i := 0; i < *count; i++ {
+		time.Sleep(delay_duration)
+		fmt.Println("Iteration:", i+1)
+	}
+}
