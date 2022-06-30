61a24362a9592ad65b043fbd3863a85855f988e1
Alec Bakholdin <alecbakholdin@gmail.com>
2022-06-30 17:43:30 -0400

    added test for ignoring default and deleted tests that tested functionality of defaults for flags

--- argparse_test.go
+++ argparse_test.go
@@ -2042,12 +2042,12 @@ func TestStringDefaultValueFail(t *testing.T) {
 
 	p := NewParser("progname", "Prog description")
 
-	_ = p.String("s", "string", &Options{Default: false})
+	_ = p.String("s", "string", &Options{Default: true})
 
 	err := p.Parse(testArgs)
 
 	// Should pass on failure
-	if err == nil || err.Error() != "cannot use default type [bool] as value of pointer with type [*string]" {
+	if err == nil || err.Error() != "argument [s, string] does not support default values" {
 		t.Errorf("Test %s failed: expected error [%s], got error [%+v]", t.Name(), "cannot use default type [bool] as value of pointer with type [*string]", err)
 	}
 }
